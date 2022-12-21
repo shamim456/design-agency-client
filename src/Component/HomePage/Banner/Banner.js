b0714775f4785cabcd287cd6c00b269d89b9b549
shamim <shamimhossen3912@gmail.com>
2022-12-21 09:33:57 +0600

    config tailwind & daisy ui

--- src/Component/HomePage/Banner/Banner.js
+++ src/Component/HomePage/Banner/Banner.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+import "./Banner.css";
+
+// import required modules
+import { Keyboard, Pagination, Navigation } from "swiper";
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Keyboard, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ducimus asperiores aspernatur quasi aliquam voluptatem eveniet perspiciatis dolorem consequuntur reprehenderit, ipsa officiis voluptas, corporis ipsam architecto molestiae sunt, fugiat odio.</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Banner;
